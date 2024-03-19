c2afe4d2451155c9fd291d39c4d35b2df0478e95
Yuvraj Goswami <133553728+Yuvraj-8182@users.noreply.github.com>
2024-03-19 21:17:42 +0530

    Add files via upload
    express js

---
+++ index.js
@@ -0,0 +1,14 @@
+const http=require("http");
+const express=require("express");
+
+const app=express();
+
+app.get("/",(req,res)=>{
+    return res.send("hello yuvraj");
+});
+
+app.get("/about",(req,res)=>{
+    return res.send("hello yuvraj "+"hyy haru" +req.query.name);
+});
+
+app.listen(8000,()=>console.log("server started"));
